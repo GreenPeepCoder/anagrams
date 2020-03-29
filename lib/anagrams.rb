19d9165ecc433e240431db61505e392ff9ba890f
william_brooks_miller <brooksmiller.miller@gmail.com>
2020-03-29 16:02:50 -0400

    Complete Phase IV - finish project

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,4 +1,5 @@
 # Phase I
+# since we are working with permutations here, time complexity is bad: O(n!)
 def first_anagram?(str1, str2)
     perms = get_perms(str1)
     perms.include?(str2)
@@ -9,6 +10,7 @@ def get_perms(str)
 end
 
 # Phase II
+# I think the time complexity for this one is O(n) -- just itter through a loop
 def second_anagram?(str1, str2)
     return false if str1.length != str2.length
 
@@ -18,4 +20,23 @@ def second_anagram?(str1, str2)
         end
     end
     str2.empty? ? true : false
+end
+
+# Phase III
+def third_anagram?(str1, str2)
+    str1.split("").sort!.join() == str2.split("").sort!.join()
+end
+
+# Phase IV
+def fourth_anagram?(str1, str2)
+    h1 = Hash.new(0)
+    h2 = Hash.new(0)
+    str1.each_char do |char|
+        h1[char] += 1
+    end
+    str2.each_char do |char|
+        h2[char] += 1
+    end
+
+    h1 == h2 ? true : false
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -30,7 +30,15 @@ describe "#second_anagram?" do
 end
 
 describe "#third_anagram?" do
+    it 'should return true if the two words are anagrams' do
+        expect(third_anagram?("elvis", "lives")).to be(true)
+        expect(third_anagram?("gizmo", "sally")).to be(false)
+    end
 end
 
 describe "#fourth_anagram?" do
+    it 'should return true if the two words are anagrams' do
+        expect(fourth_anagram?("elvis", "lives")).to be(true)
+        expect(fourth_anagram?("gizmo", "sally")).to be(false)
+    end
 end
